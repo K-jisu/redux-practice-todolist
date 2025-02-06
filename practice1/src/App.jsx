63495c6d7ca66e7d5dd4cfe87e751655c6b15aa0
K-jisu <wltn7star@naver.com>
2025-02-06 20:30:02 +0900

    second Todo List

---
+++ practice1/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import TodoForm from "./componenet/TodoForm";
+import TodoList from "./componenet/TodoList";
+
+function App() {
+  return (
+    <>
+      <TodoForm />
+      <TodoList />
+    </>
+  );
+}
+
+export default App;
